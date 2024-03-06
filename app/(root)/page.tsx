54fc5cf63356c039139c27cb2fdc8d3705a6d210
Sumeet Shelar <sumeetshelar2000@gmail.com>
2024-03-06 16:14:14 +0530

    Integrated clerk for user authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl">EVZ</h1>
-      <Button variant="destructive" className="px-5">Click Me</Button>
     </main>
   );
 }
