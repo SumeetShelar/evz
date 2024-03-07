9805c3778df1e42aff0cc9e24831df1e14021965
Sumeet Shelar <sumeetshelar2000@gmail.com>
2024-03-07 13:52:39 +0530

    Add responsive Footer component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,15 +2,15 @@ import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <div>
-      <Header />
-      <main className="flex-1">{children}</main>
-      <Footer/>
-    </div>
-  );
+	return (
+		<div className="flex flex-col h-screen">
+			<Header />
+			<main className="flex-1">{children}</main>
+			<Footer />
+		</div>
+	);
 }
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,22 @@
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+import Image from "next/image";
+import Link from "next/link";
 
-export default Footer
+const Footer = () => {
+	return (
+		<footer className="border-t">
+			<div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+				<Link href="/">
+					<Image
+						src="assets/images/logo-evz.svg"
+						alt="logo"
+						width={128}
+						height={38}
+					/>
+				</Link>
+				<p>2024 EVZ. All right reserved.</p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
