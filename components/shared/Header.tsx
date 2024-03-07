3bc5b7854e94ae7d74888d9eb60ffc2fd47d9d3c
Sumeet Shelar <sumeetshelar2000@gmail.com>
2024-03-07 13:39:13 +0530

    Add responsive Navbar component

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,31 +1,43 @@
-import Image from "next/image"
-import Link from "next/link"
-import { Button } from "../ui/button"
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
-  return (
-    <header className="w-full border-b">
-      <div className="wrapper flex items-center justify-between">
-        <Link href='/' className="w-36">
-          <Image src='assets/images/logo-evz.svg' width={128} height={38} alt="EVZ logo" />
-        </Link>
+	return (
+		<header className="w-full border-b-2 border-gray-100">
+			<div className="wrapper flex items-center justify-between">
+				<Link href="/" className="w-36">
+					<Image
+						src="assets/images/logo-evz.svg"
+						width={128}
+						height={38}
+						alt="EVZ logo"
+					/>
+				</Link>
 
-        <div className="flex w-32 justify-end gap-3">
-          <SignedIn>
-            <UserButton afterSignOutUrl="/" />
-          </SignedIn>
-          <SignedOut>
-            <Button asChild className="rounded-full" size="lg">
-              <Link href='/sign-in'>
-                Login
-              </Link>
-            </Button>
-          </SignedOut>
-        </div>
-      </div>
-    </header>
-  )
-}
+				<SignedIn>
+					<nav className="md:flex-between hidden w-full max-w-xs">
+						<NavItems />
+					</nav>
+				</SignedIn>
 
-export default Header
+				<div className="flex w-32 justify-end gap-3">
+					<SignedIn>
+						<UserButton afterSignOutUrl="/" />
+						<MobileNav />
+					</SignedIn>
+					<SignedOut>
+						<Button asChild className="rounded-full" size="lg">
+							<Link href="/sign-in">Login</Link>
+						</Button>
+					</SignedOut>
+				</div>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
---
+++ constants/index.ts
@@ -0,0 +1,14 @@
+export const headerLinks = [
+	{
+		name: "Home",
+		route: "/",
+	},
+	{
+		name: "Create Event",
+		route: "/events/create",
+	},
+	{
+		name: "My Profile",
+		route: "/profile",
+	},
+];
